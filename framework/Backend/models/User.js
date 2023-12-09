e145b4dde9a9d088bb04fd0eb929cf4a483a38dd
Ismael Ordoñez <118380297+JILogio@users.noreply.github.com>
2023-12-08 21:10:14 -0500

    Update User.js
    Correción del modelo User

--- framework/Backend/models/User.js
+++ framework/Backend/models/User.js
@@ -21,7 +21,6 @@ const userSchema = new mongoose.Schema({
     },
     locationID: {
         type: mongoose.Schema.Types.ObjectId,
-        required: true
     }
 },{versionKey:false,collection: 'User'});
 
